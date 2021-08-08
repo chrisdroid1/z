db35bf1610523983111d2fab5e68f516ba80a1b0
Chrisdroid1 <userbotni7@gmail.com>
2021-08-08 13:24:12 +0000

    SeD Replacer

--- DaisyXMusic/modules/userbotjoin.py
+++ DaisyXMusic/modules/userbotjoin.py
@@ -126,7 +126,7 @@ async def addcchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @kaelaVC2 to your Group and try again</b>",
+            "\n\nOr manually add @helper2 to your Group and try again</b>",
         )
         return
     await message.reply_text(
