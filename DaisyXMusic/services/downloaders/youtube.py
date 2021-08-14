f1314711313aff266a6b3638477ec9966fe612ab
Christopher <82050423+chrisdroid1@users.noreply.github.com>
2021-08-14 20:22:51 +0100

    Update youtube.py

--- DaisyXMusic/services/downloaders/youtube.py
+++ DaisyXMusic/services/downloaders/youtube.py
@@ -1,22 +1,3 @@
-# Calls Music 1 - Telegram bot for streaming audio in group calls
-# Copyright (C) 2021  Roj Serbest
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-# Modified by Inukaasith
-
-
 from os import path
 
 from youtube_dl import YoutubeDL
@@ -27,6 +8,7 @@ from DaisyXMusic.helpers.errors import DurationLimitError
 ydl_opts = {
     "format": "bestaudio/best",
     "verbose": True,
+    "addmetadata": True,
     "geo-bypass": True,
     "nocheckcertificate": True,
     "outtmpl": "downloads/%(id)s.%(ext)s",
@@ -36,6 +18,7 @@ ydl = YoutubeDL(ydl_opts)
 
 
 def download(url: str) -> str:
+    global ydl
     info = ydl.extract_info(url, False)
     duration = round(info["duration"] / 60)
 
